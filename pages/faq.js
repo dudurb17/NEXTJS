7566469155b01883d8661ddf0c33daf5ed772af2
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-02 13:42:58 -0300

    pagina nao recarrega nas navegações

--- pages/faq.js
+++ pages/faq.js
@@ -1,8 +1,9 @@
+import Link from "next/link";
 export default function FAQPagina() {
   return (
     <div>
       <h1>Alura Cases - Paginas de perguntas FAQ</h1>
-      <a href="/">Ir para a home</a>
+      <Link href="/">Ir para a home</Link>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <h1>Alura Cases- Pagina home</h1>
-      <a href="/faq">Ir para página de FAQ</a>
+      <Link href="/faq">Ir para página de FAQ</Link>
     </div>
   );
 }
