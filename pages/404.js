43f0875f11fd9b37cd8bd77cfadb618503b1a68e
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-02 13:53:24 -0300

    criando a pagina 404

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Link from "../src/components/link";
+export default function Page404() {
+  return (
+    <div>
+      <h1>Você se perdeu e caiu na página 404 :0</h1>
+      <Link href="/">Ir para a home</Link>
+    </div>
+  );
+}
