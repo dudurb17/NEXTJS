6ce4d107a7e1428b2a85d24bf317ca0e954ff29a
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-02 18:27:14 -0300

    modelando a home

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
-import { Inter } from "next/font/google";
-import Link from "../src/components/link";
-import React from "react";
+import HomeScreen from "@/src/screens/HomeSreen";
 
-const inter = Inter({ subsets: ["latin"] });
+export default HomeScreen;
+// import { Inter } from "next/font/google";
+// import Link from "../src/components/link";
+// import React from "react";
+// import Footer from "@/src/components/patterns/Footer";
 
-function Title({ children, as }) {
-  const Tag = as;
-  return (
-    <React.Fragment>
-      <Tag>{children}</Tag>
-      <style jsx>
-        {`
-          ${Tag} {
-            color: red;
-          }
-        `}
-      </style>
-    </React.Fragment>
-  );
-}
-export default function Home() {
-  return (
-    <div>
-      <Title as="h1">Alura Cases- Pagina home</Title>
-      <Link href="/faq">Ir para página de FAQ</Link>
-    </div>
-  );
-}
+// const inter = Inter({ subsets: ["latin"] });
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <React.Fragment>
+//       <Tag>{children}</Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: red;
+//           }
+//         `}
+//       </style>
+//     </React.Fragment>
+//   );
+// }
+// export default function Home() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura Cases- Pagina home</Title>
+//       <Link href="/faq">Ir para página de FAQ</Link>
+//       <Footer/>
+//     </div>
+//   );
+// }
