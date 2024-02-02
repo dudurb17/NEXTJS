629acb5f5b804e2142a8536f6923a51fa2053761
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-02 13:57:24 -0300

    mudando a cor do texto com componentes e props

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -1,5 +1,10 @@
 import NextLink from "next/link";
+import LinkEstilizado from "../LinkEstilizado";
 
 export default function Link({ children, href, ...props }) {
-  return <NextLink href={href}>{children}</NextLink>;
+  return (
+    <NextLink href={href}>
+      <LinkEstilizado {...props}>{children}</LinkEstilizado>
+    </NextLink>
+  );
 }
