219115a442b83d92d7b26b71c4ffa4fd0594d4c3
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-02 23:25:36 -0300

    requisição no servidor

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,8 @@
 import Link from "../src/components/link";
 import { useEffect, useState } from "react";
-
+// export async function getServerSideProps()  confere todo acesso que você recebe
 export async function getStaticProps() {
+  // confere somente no build
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
