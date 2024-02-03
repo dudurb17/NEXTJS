4a6486e3c80b3608d7b96bb72da7f448cd213310
dudurb17 <eduardo.robettibedin@gmail.com>
2024-02-03 00:24:25 -0300

    CSS FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,16 +1,17 @@
-import Link from "../src/components/link";
-import { useEffect, useState } from "react";
-import PageTitle from "@/src/components/PageTitle";
+// faq.js
+import FAQScreen from "../src/screens/FAQScreen";
 
+export default FAQScreen;
 // SSG - Static Site Generation
 // SSR - Server Site Redering
 // ISG - Incremental Static Generation
 
 // export async function getServerSideProps()  confere todo acesso que você recebe
 // em modo dev roda em todo acesso
+
 export async function getStaticProps() {
-  // confere somente no build
-  // em modo dev roda em todo acesso
+  console.log("Em modo DEV, sempre roda! A cada acesso");
+  console.log("Roda SOMENTE em build time");
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
@@ -23,30 +24,8 @@ export async function getStaticProps() {
 
   return {
     props: {
-      teste: "alooo",
+      qualquercoisa: "que eu passar aqui",
       faq,
     },
   };
-}
-export default function FAQPagina({ faq }) {
-  console.log(faq);
-  // const [faq, setFaq] = useState([]);
-  // useEffect(() => {}, []);
-  return (
-    <div>
-      <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
-      <h1>Alura Cases - Paginas de perguntas FAQ</h1>
-      <Link href="/">Ir para a home</Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li key={question}>
-            <article>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
